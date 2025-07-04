f2d5f0209738f9aa31a40b588886e0ef6f336e45
Adityan0198 <152312999+Adityan0198@users.noreply.github.com>
2025-07-04 13:51:13 +0530

    each, eac_with methods done

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,13 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    ind = 0
+    self.my_each do |elem|
+      yield(elem, ind)
+      ind += 1
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +15,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for i in self
+      yield(i)
+    end
+    self
+  end
 end
